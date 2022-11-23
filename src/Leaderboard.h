46b5cdb04da888cc534de70e752905d408793876
dylant313 <dylant313@yahoo.com>
2022-11-23 12:54:17 -0500

    Initial commit

---
+++ src/Leaderboard.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Leaderboard
+{
+private:
+    /* data */
+public:
+    /* data */
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "Leaderboard.cpp"
+
+int main()
+{
+    /* code */
+    return 0;
+}
