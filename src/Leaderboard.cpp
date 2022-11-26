f24359a8b43796ccbc0b4c38c2e60787686feaa6
dylant313 <dylant313@yahoo.com>
2022-11-26 13:32:13 -0500

    basic class structure

--- src/Leaderboard.cpp
+++ src/Leaderboard.cpp
@@ -1,1 +1,18 @@
 #include "Leaderboard.h"
+
+Leaderboard::Leaderboard()
+{
+    // import data into map
+}
+
+void Leaderboard::mergeSort()
+{
+}
+
+void Leaderboard::radixSort()
+{
+}
+
+void Leaderboard::search(string name)
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    /* code */
+    // implement console app
     return 0;
 }
