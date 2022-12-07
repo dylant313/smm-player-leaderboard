6133dbca140a72df78caa04fe792d5ba64dd0400
Rachel Ponce <93273315+rachelponce@users.noreply.github.com>
2022-12-06 19:40:06 -0500

    Addition of Radix Sort Helper Functions

--- src/Leaderboard.h
+++ src/Leaderboard.h
@@ -13,8 +13,13 @@ private:
     // each player is represented by a string (name) and map of values (plays, clears, likes, records)
     unordered_map<string, unordered_map<string, int>> unsortedPlayers;
 
+    // Helper Functions for Merge Sort
     void mergeSortHelper(vector<pair<string, int>> &temp, int left, int right);
     void mergeHelper(vector<pair<string, int>> &temp, int left, int mid, int right);
+    
+    // Helper Functions for Radix Sort
+    int findMax(vector<pair<string, int>> &temp, int vectorSize);
+    void countSort(vector<pair<string, int>> &temp, int vectorSize, int placeValue);
 
 public:
     Leaderboard();
